5125dea25cb103b5faa7d2b67b2d4a5d4b177e13
Juan Carlos <juancarlosgq@correo.ugr.es>
2021-08-17 10:55:06 +0200

    Tercera clase

--- funciones.js
+++ funciones.js
@@ -1,19 +1,41 @@
 function Calculadora(){
-    var numeroUno, numeroDos;
 
-    numeroUno = parseFloat(document.getElementById("numeroUno").value);
-    numeroDos = parseFloat(document.getElementById("numeroDos").value);
+    numeroUno = document.getElementById("numeroUno").value;
+    numeroDos = document.getElementById("numeroDos").value;
 
-    var suma, resta, multiplicacion, division, potencia;
-    suma = numeroUno+numeroDos;
-    resta = numeroUno-numeroDos;
-    multiplicacion = numeroUno*numeroDos;
-    division = numeroUno/numeroDos;
-    potencia = Math.pow(numeroUno, numeroDos);
+    var numero = /^[0-9]$/;
+    if(!numero.test(numeroUno) || !numero.test(numeroDos)){
+        alert("Error. No has introducido un número");
+    }
+    else{
+        suma = numeroUno+numeroDos;
+        resta = numeroUno-numeroDos;
+        multiplicacion = numeroUno*numeroDos;
+        division = numeroUno/numeroDos;
+        potencia = Math.pow(numeroUno, numeroDos);
+    
+        document.getElementById("suma").value=suma;
+        document.getElementById("resta").value=resta;
+        document.getElementById("mul").value=multiplicacion;
+        document.getElementById("div").value=division;
+        document.getElementById("pot").value = potencia;
+    }
 
-    document.getElementById("suma").value=suma;
-    document.getElementById("resta").value=resta;
-    document.getElementById("mul").value=multiplicacion;
-    document.getElementById("div").value=division;
-    document.getElementById("pot").value = potencia;
+
+    
+}
+
+
+function sumar(){
+    numeroUno = parseInt(document.getElementById("numeroUno").value);
+    numeroDos = parseInt(document.getElementById("numeroDos").value);
+
+    var numero = /^[0-9]$/;
+    if(!numero.test(numeroUno) || !numero.test(numeroDos)){
+        alert("Error. No has introducido un número");
+    }
+    else{
+        suma = numeroUno + numeroDos;
+        document.getElementById("Resultado").value = suma;
+    }
 }
