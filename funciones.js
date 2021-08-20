105f8fe188012d5d0a76e093e5a9d870efdebce3
Juan Carlos <juancarlosgq@correo.ugr.es>
2021-08-20 19:18:18 +0200

    Quinta clase

--- funciones.js
+++ funciones.js
@@ -39,3 +39,47 @@ function sumar(){
         document.getElementById("Resultado").value = suma;
     }
 }
+
+
+var arrayImages = ["imagenes/pina.jpg", "imagenes/kiwi.png", "imagenes/manzana.jpg", "imagenes/melon.jpg", "imagenes/platano.jpg", "imagenes/sandia.jpg", "imagenes/uva.jpg", "imagenes/melocoton.jpg", "imagenes/pina.jpg", "imagenes/kiwi.png", "imagenes/manzana.jpg", "imagenes/melon.jpg", "imagenes/platano.jpg", "imagenes/sandia.jpg", "imagenes/uva.jpg", "imagenes/melocoton.jpg"];
+var cliclado=false;
+var srcClicado = "imagen/pina.jpg";
+var idClicado = "tres";
+var puntos = 0;
+
+function voltarImagen(identificador){
+    document.getElementById(identificador).src=arrayImages[parseInt(identificador)];
+    setTimeout(() => {
+        console.log(comprobarPuntos(identificador))
+      }, 1000);
+    
+}
+
+function comprobarPuntos(identificador){
+    if(cliclado == false){
+        cliclado = true;
+        srcClicado = arrayImages[parseInt(identificador)];
+        idClicado = identificador;
+    }
+    else{
+        if(srcClicado == arrayImages[parseInt(identificador)]){
+            alert("Pareja correcta");
+            puntos += 100;
+            cliclado=false;
+        }
+        else{
+            puntos -= 50;
+            alert("Error, pareja incorrecta");
+            document.getElementById(identificador).src = "imagenes/interrogacion.jpg";
+            document.getElementById(idClicado).src = "imagenes/interrogacion.jpg";
+            cliclado=false;
+        }
+    }
+}
+
+function reiniciarJuego(){
+    arrayImages.sort(function(){
+        return Math.random() - 0.55
+    });
+}
+
