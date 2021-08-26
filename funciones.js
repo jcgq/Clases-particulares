2b0c5a6b768585a08d285f6a958c62663503398b
Juan Carlos <juancarlosgq@correo.ugr.es>
2021-08-26 10:48:20 +0200

    Octava clase

--- funciones.js
+++ funciones.js
@@ -19,14 +19,11 @@ function Calculadora(){
         document.getElementById("mul").value=multiplicacion;
         document.getElementById("div").value=division;
         document.getElementById("pot").value = potencia;
-    }
-
-
-    
+    }    
 }
 
-
 function sumar(){
+
     numeroUno = parseInt(document.getElementById("numeroUno").value);
     numeroDos = parseInt(document.getElementById("numeroDos").value);
 
@@ -38,6 +35,7 @@ function sumar(){
         suma = numeroUno + numeroDos;
         document.getElementById("Resultado").value = suma;
     }
+    
 }
 
 
@@ -107,4 +105,151 @@ var n = 0;
 function contarSegundos(){
     document.getElementById("contador").value = n;
     n++;
+}
+
+
+//****************************************************************************/
+
+
+
+  
+
+  var temas = new Map();
+  temas.set(0, 'Deportes');
+  temas.set(1, 'Biologia');
+  temas.set(2, 'Sociales');
+  temas.set(3, 'Literatura');
+  temas.set(4, 'Musica');
+  temas.set(5, 'Historia');
+
+
+  var preguntasDeporte = new Map();
+  preguntasDeporte.set(0, '¿Cuantas personas juegan en un partido de baloncesto?');
+  preguntasDeporte.set(1, '¿Cuantas personas juegan en un partido de futbol?');
+  preguntasDeporte.set(2, '¿Cuantas personas juegan en un partido de balonmano?');
+
+  var respuestasDeporte = new Map();
+  respuestasDeporte.set(0, '5');
+  respuestasDeporte.set(1, '11');
+  respuestasDeporte.set(2, '7');
+
+  //------------------------------------------------------------------------------
+
+  var preguntasBiologicas = new Map();
+  preguntasBiologicas.set(0 , 'Eucariotas, ¿es un tipo de célula?');
+
+  var respuestasBiologicas = new Map();
+  respuestasBiologicas.set(0 , 'si');
+
+  //------------------------------------------------------------------------------
+
+  var preguntasSociales = new Map();
+  preguntasSociales.set(0 , '¿Quién es el jugador de mayor edad en el Betis CF?');
+
+  var respuestasSociales = new Map();
+  respuestasSociales.set(0 , 'Joaquin');
+
+    //------------------------------------------------------------------------------
+
+  var preguntasLiteratura = new Map();
+  preguntasLiteratura.set(0 , 'Pedro López, ¿es un famoso escritor?');
+
+  var respuestasLiteratura = new Map();
+  respuestasLiteratura.set(0 , 'no');
+
+    //------------------------------------------------------------------------------
+
+  var preguntasMusica = new Map();
+  preguntasMusica.set(0 , 'Pedro López, ¿es un famoso escritor?');
+
+  var respuestasMusica = new Map();
+  respuestasMusica.set(0 , 'no');
+
+    //------------------------------------------------------------------------------
+
+  var preguntasHistoria = new Map();
+  preguntasHistoria.set(0 , '¿Cuantos componían la quinta Estación?');
+
+  var respuestasHistoria = new Map();
+  respuestasHistoria.set(0 , '4');
+
+    //------------------------------------------------------------------------------
+
+function girarRuleta(){
+    var numero = parseInt(Math.random() * 5);
+    var temaElegido = temas.get(numero);
+    alert("El tema de la pregunta será " + temaElegido);
+
+    responder(temaElegido);
+}
+
+
+
+function responder(temaElegido){
+    
+    if(temaElegido == "Deportes"){
+        var numPregunta = parseInt(Math.random() * preguntasDeporte.size);
+        var pregunta = preguntasDeporte.get(numPregunta);
+        //alert("La pregunta es: " + pregunta);
+
+        var solucion = prompt(pregunta);
+        if(solucion == respuestasDeporte.get(numPregunta)){
+            alert("Acertaste");
+        }
+        else{
+            alert("Pierdes una vida");
+            comprobarVida();
+        }
+    }
+
+    if(temaElegido == "Literatura"){
+        var numPregunta = parseInt(Math.random() * preguntasLiteratura.size);
+        var pregunta = preguntasLiteratura.get(numPregunta);
+        alert("La pregunta es: " + pregunta);
+    }
+
+    if(temaElegido == "Biologia"){
+        var numPregunta = parseInt(Math.random() * preguntasBiologia.size);
+        var pregunta = preguntasBiologia.get(numPregunta);
+        alert("La pregunta es: " + pregunta);
+    }
+
+    if(temaElegido == "Sociales"){
+        var numPregunta = parseInt(Math.random() * preguntasSociales.size);
+        var pregunta = preguntasSociales.get(numPregunta);
+        alert("La pregunta es: " + pregunta);
+    }
+
+    if(temaElegido == "Musica"){
+        var numPregunta = parseInt(Math.random() * preguntasMusica.size);
+        var pregunta = preguntasMusica.get(numPregunta);
+        alert("La pregunta es: " + pregunta);
+    }
+
+    if(temaElegido == "Historia"){
+        var numPregunta = parseInt(Math.random() * preguntasHistoria.size);
+        var pregunta = preguntasHistoria.get(numPregunta);
+        alert("La pregunta es: " + pregunta);
+    }
+
+}
+
+
+function comprobarVida(){
+    var srcVida1 = document.getElementById("vida1").src;
+    if(srcVida1 == "imagenes/vidaPerdida.jpg"){
+        var srcVida2 = document.getElementById("vida2").src;
+        if(srcVida2 == "imagenes/vidaPerdida.jpg"){
+            var srcVida3 = document.getElementById("vida3").src;
+            if(srcVida3 == "imagenes/vidaPerdida.jpg"){
+                document.getElementById("vida3").src = "imagenes/vidaPerdida.jpg";
+            }
+        }
+        else{
+            document.getElementById("vida2").src = "imagenes/vidaPerdida.jpg";
+        }
+    }
+    else{
+        document.getElementById("vida1").src = "imagenes/vidaPerdida.jpg";
+    }
 }
